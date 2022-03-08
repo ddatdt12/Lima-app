c101cb043ad9c94478c4c9984512cc18a5a28a71
TranDKhoi <tranxyz88@gmail.com>
2022-03-08 14:44:49 +0700

    thêm assets và set up MainWindow

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace LibraryManagement.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+        public MainWindowViewModel()
+        {
+        }
+
+
+    }
+}
