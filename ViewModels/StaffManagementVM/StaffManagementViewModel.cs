c281d35debb718891bd8b51104d62284d282b298
Lê Hải Phong <75621558+HaiPhong146@users.noreply.github.com>
2022-04-11 22:55:02 +0700

    Xong page quản lý nhân viên chờ UI thêm nv

--- ViewModels/StaffManagementVM/StaffManagementViewModel.cs
+++ ViewModels/StaffManagementVM/StaffManagementViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagement.DTOs;
+using LibraryManagement.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,37 +9,19 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.ViewModels.StaffManagementVM
 {
-    public class Staff
-    {
-        public string idStaff { get; set; }
-        public string nameStaff { get; set; }
-        public string birthDay { get; set; }
-        public string idCard { get; set; }
-        public string sex { get; set; }
-        public Staff()
-        {
-            idStaff = "ABCD";
-            nameStaff = "Lê Hải Phong";
-            birthDay = "20/04/2002";
-            idCard = "212454483";
-            sex = "Nam";
-        }
-    }
     public partial class StaffManagementViewModel
     {
-        private ObservableCollection<Staff> _ListStaff = new ObservableCollection<Staff>();
-
-        public ObservableCollection<Staff> ListStaff
+        private ObservableCollection<EmployeeDTO> _ListStaff;
+        public ObservableCollection<EmployeeDTO> ListStaff
         {
-            get {
-                for (int i = 0; i < 10; i++)
-                {
-                    _ListStaff.Add(new Staff());
-                }
-                return _ListStaff; 
-            }
+            get { return _ListStaff; }
             set { _ListStaff = value; }
         }
 
+
+        public StaffManagementViewModel()
+        {
+            ListStaff = new ObservableCollection<EmployeeDTO>(EmployeeService.Ins.GetAllEmployees());
+        }
     }
 }
